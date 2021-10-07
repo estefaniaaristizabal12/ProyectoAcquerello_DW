fbdf7ec9ec5dda243aaa2905300883e8f6c518d7
estefaniaaristizabal12 <estefaniaaristizabal12@hotmail.com>
2021-10-07 11:31:34 -0500

    base datos

--- ProyectoAcquerello/src/main/java/com/desarrolloWeb/ProyectoAcquerello/ProyectoAcquerelloApplication.java
+++ ProyectoAcquerello/src/main/java/com/desarrolloWeb/ProyectoAcquerello/ProyectoAcquerelloApplication.java
@@ -2,8 +2,10 @@ package com.desarrolloWeb.ProyectoAcquerello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ProyectoAcquerelloApplication {
 
 	public static void main(String[] args) {
